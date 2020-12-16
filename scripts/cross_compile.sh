91e9440454f9bec60e130c433998380f04c99bb2
Simon Sørensen <hello@simse.io>
2020-12-16 20:14:01 +0100

    updated CI structure

---
+++ scripts/cross_compile.sh
@@ -0,0 +1,25 @@
+package="github.com/simse/qc"
+package_split=(${package//\// })
+package_name=${package_split[-1]}
+
+platforms=("windows/amd64" "darwin/amd64" "linux/amd64" "darwin/arm64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+
+    output_name='build/'$package_name'-'$GOOS'-'$GOARCH
+
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name -ldflags "-X github.com/simse/qc/cmd.Version=${VERSION}" $package
+
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
