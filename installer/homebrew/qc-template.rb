2097aeaa7b000728c457a07aa5580d35369172a8
Simon Sorensen <hello@simse.io>
2021-08-26 11:08:57 +0000

    fix homebrew template styling

--- installer/homebrew/qc-template.rb
+++ installer/homebrew/qc-template.rb
@@ -1,11 +1,11 @@
 class Qc < Formula
-    desc "qc is a tool for converting between file formats"
+    desc "A tool for converting between file formats"
     homepage "https://qc.simse.io"
     url "{{URL}}"
   
     depends_on "go" => :build
-    depends_on "vips" => :build
     depends_on "pkg-config" => :build
+    depends_on "vips" => :build
     depends_on "vips"
   
     def install
